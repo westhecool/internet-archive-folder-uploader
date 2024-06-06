22e56eff1f726949064a2cb2d6846cf71f570bef
westhecool <52859033+westhecool@users.noreply.github.com>
2024-06-05 23:01:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 import sys
+import subprocess
 if len(sys.argv) < 3:
-    print("Usage: python3 main.py <identifier> <folder>")
+    print('Usage: python3 main.py <identifier> <folder>')
     exit(1)
 
 identifier = sys.argv[1]
@@ -11,7 +12,7 @@ def upload(directory, remote_directory):
     for filename in os.listdir(directory):
         if os.path.isfile(directory + '/' + filename):
             print(f'Uploading {directory + "/" + filename} to {(remote_directory + "/" + filename) if remote_directory else filename}')
-            os.system(f'ia upload {identifier} "{directory + "/" + filename}" --remote-name="{(remote_directory + "/" + filename) if remote_directory else filename}"')
+            subprocess.run(['ia', 'upload', '--no-backup', identifier, directory + '/' + filename, f'--remote-name="{(remote_directory + "/" + filename) if remote_directory else filename}'])
         elif os.path.isdir(directory + '/' + filename):
             upload(directory + '/' + filename, (remote_directory + '/' + filename) if remote_directory else filename)
 upload(folder, '')
